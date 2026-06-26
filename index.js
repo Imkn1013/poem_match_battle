b08e8083f8a5504fa952c6e736673358cbe40998
Imkn1013 <imkn.tr700602@gmail.com>
2026-06-26 14:40:20 +0000

    changed

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
 const express = require('express');
-const app = express();
+const http = require('http');
+const { Server } = require('socket.io');
+//const path = require('path');
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "https://imkn1013.github.io"
+  }
 });
 
-app.listen(3000, () => {
+app.get('/', (req, res) => {
+  res.send("Socket.IO server is running.");
+});
+
+io.on("connection", (socket) => {
+  console.log("A client connected:", socket.id);
+  
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
   console.log('Server listening on port 3000');
 });
