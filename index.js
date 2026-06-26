cad033bbce1ec112a17ef14a40665dae6c4c314f
Imkn1013 <imkn.tr700602@gmail.com>
2026-06-27 03:49:47 +0900

    Change app to server for listening on port

--- index.js
+++ index.js
@@ -76,6 +76,6 @@ io.on("connection", (socket) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log('Server listening on port 3000');
 });
